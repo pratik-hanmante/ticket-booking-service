3222ba9673ae12017f9e588b74ed3167a7ba2c36
pratik <pratikhanmante3@gmail.com>
2025-02-01 23:38:22 +0530

    entities created

---
+++ app/src/main/java/ticket.booking/entities/Ticket.java
@@ -0,0 +1,11 @@
+package ticket.boooking.entities;
+
+
+public class Ticket {
+    private String ticketId;
+    private String userId;
+    private String source;
+    private String destination;
+    private Date dateOfTravel;
+    private Train trainl;
+}
---
+++ app/src/main/java/ticket.booking/entities/Train.java
@@ -0,0 +1,11 @@
+package ticket.booking.entities;
+
+public class Train
+{
+    private String trainId;
+    private String trainNo;
+    private List<List<Integer>> seats;
+    private String trainId;
+    private String trainId;
+    private String trainId;
+}
