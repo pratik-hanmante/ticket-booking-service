479cdb58ac79bc1fe573213354fda6f67e98a07e
pratik-hanmante <84140896+pratik-hanmante@users.noreply.github.com>
2025-03-16 23:27:16 +0530

    Update UserBookingService.java

--- app/src/main/java/ticket/booking/services/UserBookingService.java
+++ app/src/main/java/ticket/booking/services/UserBookingService.java
@@ -11,7 +11,7 @@ public class UserBookingService
     private static final String USERS_PATH= "../localDB/users.json";
 
     public UserBookingService(User user1) {
-        //storing the user data in user1 object
+        //stores the user data in user1 object
             this.user= user1;
         File users = new File(USERS_PATH);
     }
